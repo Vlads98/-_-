f5a1252b2c824e160e4b033f35e53e62169e3126
Vlads98 <vladsavkin1998@mail.ru>
2021-12-21 22:02:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_день_недели
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число от 1 до 7");
+            string day = Console.ReadLine();
+            switch (day) 
+            {
+                case "1":
+                    Console.WriteLine("Понедельник");
+                    break;
+                case "2":
+                    Console.WriteLine("Вторник");
+                    break;
+                case "3":
+                    Console.WriteLine("Среда");
+                    break;
+                case "4":
+                    Console.WriteLine("Четверг");
+                    break;
+                case "5":
+                    Console.WriteLine("Пятница");
+                    break;
+                case "6":
+                    Console.WriteLine("Суббота");
+                    break;
+                case "7":
+                    Console.WriteLine("Воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("Вы нажали неправильную кнопку");
+                    break;
+            }
+            
+           
+        }
+    }
+}
